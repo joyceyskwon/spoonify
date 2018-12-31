bc046248af6f0d26b69f0d0c54aae5fceebf5a30
github username <github email address>
2018-12-31 10:44:22 -0500

    styled buttons

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :restlists
   has_many :lists, through: :restlists
+
+  validates :list_id, presence: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :yelp_url, presence: true
+  validates :yelp_url, uniqueness: true
 end
