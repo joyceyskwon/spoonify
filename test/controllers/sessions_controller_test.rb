ba5b0ad7b59ec63ecbf89a364563e114b922be4c
github username <github email address>
2019-01-01 18:43:07 -0500

    added some specs

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  describe 'POST #create' do
+    context 'when password is invalid' do
+      it 'renders the page with error' do
+        user = create(:user)
+        post :create, session: { username: user.username, password: 'invalid' }
+        expect(response).to render_template(:new)
+        expect(flash[:notice]).to match(/^Invalid username or password/)
+      end
+    end
+
+    context 'when password is valid' do
+      it 'sets the user in the session and redirects them to their dashboard' do
+        user = create(:user)
+        post :create, session: { username: user.username, password: user.password }
+        expect(response).to redirect_to '/user/:id'
+        expect(controller.current_user).to eq user
+      end
+    end
+  end # end of POST#create
+
+end # end of SessionsControllerTest
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  describe 'POST #create' do
+    context 'when username is already being used' do
+      it 'should render the page with error' do
+        user = create(:user)
+        post :create, user: { username: user.username, password: user.password }
+        expect(response).to render_template(:new)
+        expect(flash[:notice]).to match(/^Sorry! Username is already taken. Please try again./)
+      end
+    end
+  end
+
+end # end of UsersControllerTest
